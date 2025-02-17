cce552164dea7c7713549a2e0a0a55719278f771
Civicore Solutions <civicoresolutions@gmail.com>
2025-02-17 15:01:09 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBLUXfmLksn-DKhiBl_tULEsa4j5y7pTRY",
-  authDomain: "civicoresolutions-vanguard.firebaseapp.com",
-  projectId: "civicoresolutions-vanguard",
-  storageBucket: "civicoresolutions-vanguard.firebasestorage.app",
-  messagingSenderId: "783587797610",
-  appId: "1:783587797610:web:0b847f90b416f0ff673813"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLUXfmLksn-DKhiBl_tULEsa4j5y7pTRY",
+    authDomain: "civicoresolutions-vanguard.firebaseapp.com",
+    projectId: "civicoresolutions-vanguard",
+    storageBucket: "civicoresolutions-vanguard.firebasestorage.app",
+    messagingSenderId: "783587797610",
+    appId: "1:783587797610:web:0b847f90b416f0ff673813"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
