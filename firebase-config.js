70ddd30446b0e0cff6b99c2aa7b9aa3664e2f0df
Civicore Solutions <civicoresolutions@gmail.com>
2025-02-17 14:53:52 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLUXfmLksn-DKhiBl_tULEsa4j5y7pTRY",
+  authDomain: "civicoresolutions-vanguard.firebaseapp.com",
+  projectId: "civicoresolutions-vanguard",
+  storageBucket: "civicoresolutions-vanguard.firebasestorage.app",
+  messagingSenderId: "783587797610",
+  appId: "1:783587797610:web:0b847f90b416f0ff673813"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
